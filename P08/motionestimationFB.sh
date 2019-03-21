bfaf3346a5f392205e9a9aafef0c7295948f649f
antongoransson <anton.goransson95@gmail.com>
2019-03-21 15:40:36 +0000

    P08 done

--- P08/motionestimationFB.sh
+++ P08/motionestimationFB.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
- 
+
+# Fixed bitrate
 meMethod=("dia"  "hex"  "umh"  "full"  "tesa")
 
 FFCMD="ffmpeg -hide_banner -y -benchmark -i $1 -an -pix_fmt yuv420p -c:v libx264 -b:v 2M -maxrate 2M -bufsize 2M -bf 2 "
   
 for index in ${!meMethod[*]}; do
      mthd=${meMethod[$index]}
-     (time $FFCMD -motion-est $index test-$mthd.mov) 2> $mthd-fb.log
+     (time $FFCMD -motion-est $index test-$mthd-fb.mov) 2> $mthd-fb.log
 done
 
--- P08/motionestimationFQ.sh
+++ P08/motionestimationFQ.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
  
+ # Fixed quality --> crf 24
 meMethod=("dia"  "hex"  "umh"  "full"  "tesa")
 
 FFCMD="ffmpeg -hide_banner -y -benchmark -i $1 -an -pix_fmt yuv420p -c:v libx264 -crf 24 -bf 2 "
   
 for index in ${!meMethod[*]}; do
      mthd=${meMethod[$index]}
-     (time $FFCMD -motion-est $index test-$mthd.mov) 2> $mthd-fq.log
+     (time $FFCMD -motion-est $index test-$mthd-fq.mov) 2> $mthd-fq.log
 done
 
