1ae11224e3e479ca211e2f378a02d9282f248b6b
antongoransson <anton.goransson95@gmail.com>
2019-03-24 16:57:09 +0000

    Fixes

--- zip.sh
+++ zip.sh
@@ -1,4 +1,4 @@
 mkdir Homeworks && \
-cp -r HW01 Homeworks && cp -r HW02 Homeworks && cp -r HW03 Homeworks &&\
+cp -r HW01 Homeworks && cp -r HW02 Homeworks && cp -r HW03 Homeworks && \
 zip -r homeworks.zip Homeworks && \
 rm -rf Homeworks
