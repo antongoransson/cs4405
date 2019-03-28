876f3b8a1a2a0a77ee1ee2700fe4ebd9071a4ac5
antongoransson <anton.goransson95@gmail.com>
2019-03-28 15:48:57 +0000

    Add more scripts

---
+++ P09/dash.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+
+lib/bento4/bin/mp4dash --no-split --use-segment-list [type=audio]audio-frag.mp4 [type=video]rep-0500k-frag.mp4 [type=video]rep-1000k-frag.mp4 [type=video]rep-2000k-frag.mp4
--- P09/fragment.sh
+++ P09/fragment.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
+# Fragment the different videos
 
-./lib/bento4/bin/mp4fragment rep-0500k.mp4 rep-0500k-frag.mp4
+lib/bento4/bin/mp4fragment rep-0500k.mp4 rep-0500k-frag.mp4
+
+lib/bento4/bin/mp4fragment rep-1000k.mp4 rep-1000k-frag.mp4
+
+lib/bento4/bin/mp4fragment rep-2000k.mp4 rep-2000k-frag.mp4
+
+# Manually set fragments
+lib/bento4/bin/mp4fragment --fragment-duration 2000 audio.mp4 audio-frag.mp4
